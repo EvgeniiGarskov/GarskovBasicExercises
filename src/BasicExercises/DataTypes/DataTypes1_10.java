89e838f793b3d42cd86487a90addd9d6e34cc8c8
Евгений <Klimber43@gmail.com>
2023-04-19 21:05:44 +0400

    DataTypes

--- src/BasicExercises/DataTypes/DataTypes1_10.java
+++ src/BasicExercises/DataTypes/DataTypes1_10.java
@@ -1,5 +1,7 @@
 package BasicExercises.DataTypes;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class DataTypes1_10 {
@@ -70,17 +72,138 @@ public class DataTypes1_10 {
 
         System.out.println("Exercise 5");
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Input the time zone offset to GMT: ");
-        long timeZoneChange = input.nextInt();
-        long totalMilliseconds = System.currentTimeMillis();
-        long totalSeconds = totalMilliseconds / 1000;
-        long currentSecond = totalSeconds % 60;
-        long totalMinutes = totalSeconds / 60;
-        long currentMinute = totalMinutes % 60;
-        long totalHours = totalMinutes / 60;
-        long currentHour = ((totalHours + timeZoneChange) % 24);
-        System.out.println("Current time is " + currentHour + ":" + currentMinute + ":" + currentSecond);
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Input the time zone offset to GMT: ");
+//        long timeZoneChange = input.nextInt();
+//        long totalMilliseconds = System.currentTimeMillis();
+//        long totalSeconds = totalMilliseconds / 1000;
+//        long currentSecond = totalSeconds % 60;
+//        long totalMinutes = totalSeconds / 60;
+//        long currentMinute = totalMinutes % 60;
+//        long totalHours = totalMinutes / 60;
+//        long currentHour = ((totalHours + timeZoneChange) % 24);
+//        System.out.println("Current time is " + currentHour + ":" + currentMinute + ":" + currentSecond);
+
+        System.out.println("Exercise 6");
+
+        //Мой вариант
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Input weight in pounds: ");
+//        double weight = sc.nextDouble();
+//        System.out.println("Input height in inches: ");
+//        double height = sc.nextDouble();
+//        double BMI = (weight / Math.pow(height, 2)) * 703;
+//        System.out.println("BMI: " + BMI);
+
+        //Вариант 2
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("Input weight in pounds: ");
+//        double weight = input.nextDouble();
+//        System.out.print("Input height in inches: ");
+//        double inches = input.nextDouble();
+//        double BMI = weight * 0.45359237 / (inches * 0.0254 * inches * 0.0254);
+//        System.out.print("Body Mass Index is " + BMI+"\n");
+
+        System.out.println("Exercise 7");
+
+        //Мой вариант
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the distance in meters: ");
+//        double distance = sc.nextDouble();
+//        System.out.println("Enter hours: ");
+//        double hours = sc.nextDouble();
+//        System.out.println("Enter minutes: ");
+//        double minutes = sc.nextDouble();
+//        System.out.println("Enter seconds: ");
+//        double seconds = sc.nextDouble();
+//
+//        double justSeconds = hours * 3600 + minutes * 60 + seconds;
+//
+//        double metPerSec = distance / justSeconds;
+//        double kilPerHour = (distance / 1000) / (justSeconds / 3600);
+//        double milPerHour = kilPerHour / 1.609;
+//
+//        System.out.print("metPerSec: " + metPerSec + "\n" + "kilPerHour: " + kilPerHour + "\n" + "milPerHour: " + milPerHour + "\n");
+
+        //Вариант 2
+//        Scanner scanner = new Scanner(System.in);
+//        float timeSeconds;
+//        float mps,kph, mph;
+//        System.out.print("Input distance in meters: ");
+//        float distance = scanner.nextFloat();
+//        System.out.print("Input hour: ");
+//        float hr = scanner.nextFloat();
+//        System.out.print("Input minutes: ");
+//        float min = scanner.nextFloat();
+//        System.out.print("Input seconds: ");
+//        float sec = scanner.nextFloat();
+//        timeSeconds = (hr*3600) + (min*60) + sec;
+//        mps = distance / timeSeconds;
+//        kph = ( distance/1000.0f ) / ( timeSeconds/3600.0f );
+//        mph = kph / 1.609f;
+//        System.out.println("Your speed in meters/second is "+mps);
+//        System.out.println("Your speed in km/h is "+kph);
+//        System.out.println("Your speed in miles/h is "+mph);
+//
+//        scanner.close();
+
+        System.out.println("Exercise 8");
+
+        //Мой вариант
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the side length value: ");
+//        double num = sc.nextInt();
+//
+//        double square = Math.pow(num, 2);
+//        double cube = Math.pow(num, 3);
+//        double fourthDegree = Math.pow(num, 4);
+//
+//        System.out.println("Square: " + square + "\n" + "Cube: " + cube + "\n" + "Fourth Degree: " + fourthDegree);
+
+        //Вариант 2
+//        Scanner in = new Scanner(System.in);
+//        System.out.print("Input the side length value: ");
+//        double val = in.nextDouble();
+//        System.out.printf("Square: %12.2f\n", val * val);
+//        System.out.printf("Cube: %14.2f\n", val * val * val);
+//        System.out.printf("Fourth power: %6.2f\n", Math.pow(val, 4));
+
+        System.out.println("Exercise 9");
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the first integer: ");
+//        int num1 = sc.nextInt();
+//        System.out.println("Enter the second integer: ");
+//        int num2 = sc.nextInt();
+//
+//        System.out.printf("Sum of two integers: %d%n", num1 + num2);
+//        System.out.printf("Difference of two integers: %d%n", num1 - num2);
+//        System.out.printf("Product of two integers: %d%n", num1 * num2);
+//        System.out.printf("Average of two integers: %.2f%n", (double) (num1 + num2) / 2);
+//        System.out.printf("Distance of two integers: %d%n", Math.abs(num1 - num2));
+//        System.out.printf("Max integer: %d%n", Math.max(num1, num2));
+//        System.out.printf("Min integer: %d%n", Math.min(num1, num2));
+
+        System.out.println("Exercise 10");
+
+        //Мой вариант
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter six non-negative digits: ");
+//        int num = sc.nextInt();
+//
+//        splitTheNumber(num);
+
+        //Вариант 2
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input six non-negative digits: ");
+        int input = in.nextInt();
+        int n1 = input / 100000 % 10;
+        int n2 = input / 10000 % 10;
+        int n3 = input / 1000 % 10;
+        int n4 = input / 100 % 10;
+        int n5 = input / 10 % 10;
+        int n6 = input % 10;
+        System.out.println(n1 + " " + n2 + " " + n3 + " " + n4 + " " + n5 + " " + n6);
     }
 
 //    public static int convDegrees(int Num1, int c) {
@@ -96,5 +219,16 @@ public class DataTypes1_10 {
 //            num1 /= 10;
 //        }
 //        return sum;
+//    }
+
+//    public static int splitTheNumber(int num) {
+//        ArrayList<Integer> result = new ArrayList<Integer>();
+//        while (num > 0) {
+//            result.add(num % 10);
+//            num = num / 10;
+//        }
+//        Collections.reverse(result);
+//        System.out.println("Split number: " + result);
+//        return num;
 //    }
 }
