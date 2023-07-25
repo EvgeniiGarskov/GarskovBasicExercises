c6795ebb508f44e1fcdf6732673d3c5d721d2d83
Евгений <Klimber43@gmail.com>
2023-07-25 22:02:52 +0400

    ArrayList

---
+++ src/BasicExercises/Collection/ArrayList/ArrayList1_5.java
@@ -0,0 +1,181 @@
+package BasicExercises.Collection.ArrayList;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ArrayList1_5 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Exercise 1");
+
+        /*
+        Напишите программу на Java, чтобы создать список массивов, добавить несколько цветов (строк) и распечатать коллекцию.
+         */
+
+        //Мой вариант
+
+//        ArrayList <String> array = new ArrayList <String>();
+//        array.add("Red");
+//        array.add("Green");
+//        array.add("Orange");
+//        array.add("White");
+//        array.add("Black");
+//        System.out.println(array);
+
+        //Вариант 2
+
+//        List<String> list_Strings = new ArrayList<String>();
+//        list_Strings.add("Red");
+//        list_Strings.add("Green");
+//        list_Strings.add("Orange");
+//        list_Strings.add("White");
+//        list_Strings.add("Black");
+//        System.out.println(list_Strings);
+
+        System.out.println("Exercise 2");
+
+        /*
+        Напишите программу Java для перебора всех элементов в списке массивов.
+         */
+
+        //Мой вариант
+
+//        ArrayList <String> array = new ArrayList <String>();
+//        array.add("Red");
+//        array.add("Green");
+//        array.add("Orange");
+//        array.add("White");
+//        array.add("Black");
+//        // System.out.println(array);
+//
+//        //Вар.1
+//        // for (int i = 0; i < array.size(); i++) {
+//        //   System.out.print(array.get(i) + "; ");
+//        // }
+//
+//        //Вар.2
+//        for (String s : array) {
+//            System.out.print(s + "; ");
+//        }
+
+        //Вариант 2
+
+//        // Creae a list and add some colors to the list
+//        List<String> list_Strings = new ArrayList<String>();
+//        list_Strings.add("Red");
+//        list_Strings.add("Green");
+//        list_Strings.add("Orange");
+//        list_Strings.add("White");
+//        list_Strings.add("Black");
+//        // Print the list
+//        for (String element : list_Strings) {
+//            System.out.println(element);
+//        }
+
+        System.out.println("Exercise 3");
+
+        /*
+        Напишите программу на Java, чтобы вставить элемент в список массивов в первую позицию.
+         */
+
+        //Мой вариант
+
+//        ArrayList <String> array = new ArrayList <String>();
+//        array.add("Red");
+//        array.add("Green");
+//        array.add("Orange");
+//        array.add("White");
+//        array.add("Black");
+//        System.out.println(array);
+//
+//        array.add(0, "Pink");
+//        System.out.println(array);
+
+        //Вариант 2
+
+//        // Creae a list and add some colors to the list
+//        List<String> list_Strings = new ArrayList<String>();
+//        list_Strings.add("Red");
+//        list_Strings.add("Green");
+//        list_Strings.add("Orange");
+//        list_Strings.add("White");
+//        list_Strings.add("Black");
+//        // Print the list
+//        System.out.println(list_Strings);
+//        // Now insert a color at the first and last position of the list
+//        list_Strings.add(0, "Pink");
+//        list_Strings.add(5, "Yellow");
+//        // Print the list
+//        System.out.println(list_Strings);
+
+        System.out.println("Exercise 4");
+
+        /*
+        Напишите программу Java для извлечения элемента (по указанному индексу) из заданного списка массивов.
+         */
+
+        //Мой вариант
+
+//        ArrayList <String> array = new ArrayList <String>();
+//        array.add("Red");
+//        array.add("Green");
+//        array.add("Orange");
+//        array.add("White");
+//        array.add("Black");
+//        System.out.println(array);
+//        System.out.println("The third element: " + array.get(2));
+
+        //Вариант 2
+
+//        // Creae a list and add some colors to the list
+//        List<String> list_Strings = new ArrayList<String>();
+//        list_Strings.add("Red");
+//        list_Strings.add("Green");
+//        list_Strings.add("Orange");
+//        list_Strings.add("White");
+//        list_Strings.add("Black");
+//        // Print the list
+//        System.out.println(list_Strings);
+//        // Retrive the first and third element
+//        String element = list_Strings.get(0);
+//        System.out.println("First element: "+element);
+//        element = list_Strings.get(2);
+//        System.out.println("Third element: "+element);
+
+        System.out.println("Exercise 5");
+
+        /*
+        Напишите программу на Java для обновления элемента массива заданным элементом.
+         */
+
+        //Мой вариант
+
+//        ArrayList <String> array = new ArrayList <String>();
+//        array.add("Red");
+//        array.add("Green");
+//        array.add("Orange");
+//        array.add("White");
+//        array.add("Black");
+//        System.out.println(array);
+//
+//        array.set(2, "Pink");
+//        System.out.println(array);
+
+        //Вариант 2
+
+//        // Creae a list and add some colors to the list
+//        List<String> list_Strings = new ArrayList<String>();
+//        list_Strings.add("Red");
+//        list_Strings.add("Green");
+//        list_Strings.add("Orange");
+//        list_Strings.add("White");
+//        list_Strings.add("Black");
+//        // Print the list
+//        System.out.println(list_Strings);
+//        // Update the third element with "Yellow"
+//        list_Strings.set(2, "Yellow");
+//        // Print the list again
+//        System.out.println(list_Strings);
+    }
+}
--- src/BasicExercises/String/String38.java
+++ src/BasicExercises/String/String38.java
@@ -11,40 +11,40 @@ public class String38 {
 
     //Вариант 2
 
-    private static String removeDuplicateChars(String sourceStr) {
-        char[] arr1 = sourceStr.toCharArray();
-        String targetStr = "";
-        for (char value: arr1) {
-            if (targetStr.indexOf(value) == -1) {
-                targetStr += value;
-            }
-        }
-        return targetStr;
-    }
-
-    public static void main(String[] args) {
-
-        //Мой вариант
-
-//        String str = "w3resource";
-//        System.out.println(str);
-//        char[] ch = str.toCharArray();
-//
-//        Set<Character> charSet = new LinkedHashSet<Character>();
-//        for (char c : ch) {
-//            charSet.add(c);
+//    private static String removeDuplicateChars(String sourceStr) {
+//        char[] arr1 = sourceStr.toCharArray();
+//        String targetStr = "";
+//        for (char value: arr1) {
+//            if (targetStr.indexOf(value) == -1) {
+//                targetStr += value;
+//            }
 //        }
+//        return targetStr;
+//    }
 //
-//        StringBuilder sb = new StringBuilder();
-//        for (Character character : charSet) {
-//            sb.append(character);
-//        }
-//        System.out.println(sb.toString());
-
-        //Вариант 2
-
-        String str1 = "w3resource";
-        System.out.println("The given string is: " + str1);
-        System.out.println("After removing duplicates characters the new string is: " + removeDuplicateChars(str1));
-    }
+//    public static void main(String[] args) {
+//
+//        //Мой вариант
+//
+////        String str = "w3resource";
+////        System.out.println(str);
+////        char[] ch = str.toCharArray();
+////
+////        Set<Character> charSet = new LinkedHashSet<Character>();
+////        for (char c : ch) {
+////            charSet.add(c);
+////        }
+////
+////        StringBuilder sb = new StringBuilder();
+////        for (Character character : charSet) {
+////            sb.append(character);
+////        }
+////        System.out.println(sb.toString());
+//
+//        //Вариант 2
+//
+//        String str1 = "w3resource";
+//        System.out.println("The given string is: " + str1);
+//        System.out.println("After removing duplicates characters the new string is: " + removeDuplicateChars(str1));
+//    }
 }
