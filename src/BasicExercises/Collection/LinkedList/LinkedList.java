5d20f7867ee02e2d273d18d0f114bdc40517d4cc
Евгений <Klimber43@gmail.com>
2023-07-27 22:28:47 +0400

    LinkedList

---
+++ src/BasicExercises/Collection/LinkedList/LinkedList.java
@@ -0,0 +1,8 @@
+package BasicExercises.Collection.LinkedList;
+
+public class LinkedList {
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/BasicExercises/Main.java
+++ src/BasicExercises/Main.java
@@ -1,7 +1,6 @@
 package BasicExercises;
 
 public class Main {
-
     public static void main(String[] args) {
     }
 }
