4be1bdff77d4ceff4b9fb4c04806040efeee84f8
Евгений <Klimber43@gmail.com>
2023-08-09 22:20:09 +0400

    Enum

---
+++ src/BasicExercises/Enum/Enum3.java
@@ -0,0 +1,55 @@
+package BasicExercises.Enum;
+
+public class Enum3 {
+
+    //Мой вариант
+
+//        enum Shape {
+//            Circle,
+//            Triangle,
+//            Square
+//        }
+
+    //Вариант 2
+
+//    public enum Shape {
+//        SQUARE,
+//        CIRCLE,
+//        RECTANGLE,
+//        TRIANGLE,
+//        HEXAGON
+//    }
+
+    public static void main(String[] args) {
+
+        /*
+        Напишите программу на Java для реализации перечисления под названием «Shape» с константами, представляющими
+        различные геометрические фигуры.
+
+        Объяснение:
+
+        Мы реализуем перечисление Shape с пятью значениями, представляющими геометрические фигуры:
+        CIRCLE, SQUARE, TRIANGLE, RECTANGLE и HEXAGON. Мы создаем две переменные, shape1 и shape2, оба типа Shape, и
+        присваиваем им значения CIRCLE и SQUARE. Наконец, мы печатаем значения shape1 и shape2,
+        используя System.out.println().
+         */
+
+        //Мой вариант
+
+//        Shape circle = Shape.Circle;
+//        Shape triangle = Shape.Triangle;
+//        Shape square = Shape.Square;
+//
+//        System.out.println("Figure 1: " + circle);
+//        System.out.println("Figure 2: " + triangle);
+//        System.out.println("Figure 3: " + square);
+
+        //Вариант 2
+
+//        Shape shape1 = Shape.CIRCLE;
+//        Shape shape2 = Shape.SQUARE;
+//
+//        System.out.println("First shape: " + shape1);
+//        System.out.println("Second shape: " + shape2);
+    }
+}
---
+++ src/BasicExercises/Enum/Enum5.java
@@ -0,0 +1,63 @@
+package BasicExercises.Enum;
+
+public class Enum5 {
+
+    //Мой вариант
+//    enum Months {
+//        January,
+//        February,
+//        March,
+//        April,
+//        May,
+//        June,
+//        July,
+//        August,
+//        September,
+//        October,
+//        November,
+//        December
+//    }
+
+    //Вариант 2
+
+//    public enum Months {
+//        JANUARY,
+//        FEBRUARY,
+//        MARCH,
+//        APRIL,
+//        MAY,
+//        JUNE,
+//        JULY,
+//        AUGUST,
+//        SEPTEMBER,
+//        OCTOBER,
+//        NOVEMBER,
+//        DECEMBER
+//    }
+
+    public static void main(String[] args) {
+
+        /*
+        Напишите программу на Java, которая определяет перечисление под названием «Месяцы» с константами,
+        представляющими месяцы года.
+
+        Объяснение:
+
+        Вышеприведенная программа определяет перечисление Months с двенадцатью значениями, представляющими месяцы года:
+        ЯНВАРЬ, ФЕВРАЛЬ, МАРТ, АПРЕЛЬ, МАЙ, ИЮНЬ, ИЮЛЬ, АВГУСТ, СЕНТЯБРЬ, ОКТЯБРЬ, НОЯБРЬ и ДЕКАБРЬ. Внутри основного
+        метода мы создаем переменную currentMonth типа Months и присваиваем ей значение MAY. Наконец, мы печатаем
+        значение currentMonth, используя System.out.println().
+         */
+
+        //Мой вариант
+
+//        Months month = Months.June;
+//
+//        System.out.println("Current month: " + month);
+
+        //Вариант 2
+
+//        Months currentMonth = Months.MAY;
+//        System.out.println("Current month: " + currentMonth);
+    }
+}
