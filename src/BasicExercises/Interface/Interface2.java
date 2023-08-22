226296e6260e1d3fda77011821ff4ed77dcdb349
Евгений <Klimber43@gmail.com>
2023-08-22 19:50:57 +0400

    Interface

---
+++ src/BasicExercises/Interface/Interface2.java
@@ -0,0 +1,29 @@
+package BasicExercises.Interface;
+
+public class Interface2 {
+
+    public static void main(String[] args) {
+
+        /*
+        Напишите программу на Java для создания интерфейса Animal с методом bark(), который не принимает
+        аргументов и возвращает void. Создайте класс Dog, который реализует Animal и переопределяет функцию talk()
+        для печати «Собака лает».
+         */
+
+//        Dog dog = new Dog();
+//        dog.bark();
+    }
+}
+
+//interface Animal {
+//
+//    public void bark();
+//}
+//
+//class Dog implements Animal {
+//
+//    @Override
+//    public void bark() {
+//        System.out.println("The dog barks");
+//    }
+//}
